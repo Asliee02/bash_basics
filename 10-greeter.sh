b28a604eb332345395a10efc946612fb8ea4c062
asliee pena <apenacabrera@ggc.edu>
2026-04-08 22:23:54 -0400

    Added second argument for mood in greeter

--- 10-greeter.sh
+++ 10-greeter.sh
@@ -4,24 +4,26 @@
 
 function weekend_greet 
 {
-   echo "Enjoy the weekend! ${1}!"
+   echo "Enjoy the weekend, $1! You are feeling $2 today!"
 }
 
 function weekday_greet
 {
-   echo "Hope you are working hard, ${1}!"
+   echo "Hope you are working hard, $1! Stay $2!"
 }
 
 
 echo "Please enter your name"
 read name
+echo "How are you feeling today?"
+read mood
 
 day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
 
 if [ $day = "Sat" ] || [ $day = "Sun" ]; then
-   weekend_greet $name
+   weekend_greet $name $mood
 else
-   weekday_greet $name
+   weekday_greet $name $mood
 fi
 
 # exercise: Add a second argument to the function 
